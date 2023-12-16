0fedeed10cd5fd2c2984eb18326fe2f49bcea2cd
IamLikhon1 <likhonadnan55@gmail.com>
2023-12-16 21:35:02 +0600

    modify single page

--- src/components/AllToys/AllToys.jsx
+++ src/components/AllToys/AllToys.jsx
@@ -3,44 +3,44 @@ import AllMap from "./AllMap";
 import UseTitle from "../../hooks/UseTitle";
 
 const AllToys = () => {
-    const dataLoad=useLoaderData();
-    // console.log(dataLoad)
-    UseTitle('All Toys')
-    return (
-        <div>
-            <h2 className="text-5xl text-center text-violet-500 my-10">All Selected Toys </h2>
-            <div>
-            <div className="overflow-x-auto">
-  <table className="table w-full ">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>Seller Name</th>
-        <th>Toy Name</th>
-        <th>Sub-category</th>
-        <th>Price</th>
-        <th>Available Quantity</th>
-        <th>View Details button</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-    
-    {
-        dataLoad.map(perData=><AllMap
-        key={perData._id}
-        perData={perData}
-        ></AllMap>)
-     }
-    
-     
-      
-    </tbody>
-  </table>
-</div>
-            </div>
+  const dataLoad = useLoaderData();
+  // console.log(dataLoad)
+  UseTitle('All Toys')
+  return (
+    <div>
+      <h2 className="text-5xl text-center text-violet-500 my-10">All Selected Toys </h2>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full ">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Seller Name</th>
+                <th>Toy Name</th>
+                <th>Sub-category</th>
+                <th>Price</th>
+                <th>Available Quantity</th>
+                <th>View Details button</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+
+              {
+                dataLoad.map(perData => <AllMap
+                  key={perData._id}
+                  perData={perData}
+                ></AllMap>)
+              }
+
+
+
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
--- src/components/AllToys/SingleToy.jsx
+++ src/components/AllToys/SingleToy.jsx
@@ -1,27 +1,61 @@
 import { useLoaderData } from "react-router-dom";
+import TitleComponent from "../../NewComponents/TitleComponent/TitleComponent";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { CiHeart } from "react-icons/ci";
+import {  FaStar } from "react-icons/fa";
+import { MdOutlineToys } from "react-icons/md";
 
 const SingleToy = () => {
-    const singleData=useLoaderData();
-    const{picture,name,email,sellerName,price,rating,quantity,description}=singleData
+    const singleData = useLoaderData();
+    const { picture, name, email, sellerName, price, rating, quantity, description,category } = singleData;
+    console.log(category);
     return (
-        <div className="w-full ">
-            <div className="text-3xl text-center text-violet-500 my-10">Single Toys Information</div>
-            <div className="flex justify-center my-20"data-aos="fade-down"
-     data-aos-easing="linear"
-     data-aos-duration="1000">
-                            <div className="card card-side bg-base-100 shadow-xl">
-                <figure><img className="md:w-96" src={picture} alt="Toy Picture"/></figure>
-                <div className="card-body rounded-2xl hover:bg-sky-300">
-                    <h2 className="card-title">Toy Name: {name}</h2>
-                    <p className="mt-2 font-medium font-serif">Seller Name: {sellerName}</p>
-                    <p className="mt-2 font-medium font-serif">Seller Email: {email}</p>
-                    <p className="mt-2 font-medium font-serif">Price: ${price}</p>
-                    <p className="mt-2 font-medium font-serif">Rating: {rating}</p>
-                    <p className="mt-2 font-medium font-serif">Available Quantity: {quantity}</p>
-                    <p className="mt-2 font-medium font-serif">Detail Description: {description}</p>
-                   
+        <div className="container mx-auto">
+            <TitleComponent title="Toys Cars World" subTitle="More Information About This Toy" />
+
+            <div className="grid md:grid-cols-2 mx-5 my-10 md:my-16 gap-10 md:gap-12">
+                {/* for img */}
+                <div className="py-10 px-8 md:px-0 shadow rounded-lg border items-center">
+
+                    <img src={picture} className="md:h-96 h-full mx-auto rounded-lg" alt="" />
+
                 </div>
+
+                {/* for Text */}
+                <div className="">
+                    <h2 className="text-3xl text-start font-semibold">{name}</h2>
+                    <h4 className="text-start text-[#12AEE0] text-2xl mt-3 font-medium">${price}.00</h4>
+                    <p className="mt-5 text-[#646672] "> <span className="uppercase font-semibold">{description}</span>. <br /> <br />
+                        Dicta sunt explicabo. Nemo enim ipsam voluptatem voluptas sit odit aut fugit, sed quia consequuntur. Lorem ipsum nonum eirmod dolor. <br /> <br />
+
+                        Aquia sit amet, elitr, sed diam nonum eirmod tempor invidunt labore et dolore magna aliquyam.erat, sed diam voluptua. At vero accusam et justo duo dolores et ea rebum. Stet clitain vidunt ut labore.
+
+                    </p>
+                    {/* button */}
+
+                    <div className="mt-8 flex gap-3">
+                        <button className="px-14 py-4 rounded-full bg-[#12AEE0] text-white text-center flex items-center gap-2 font-semibold"><AiOutlineShoppingCart className="text-xl" />Buy Now</button>
+
+                        <div className="py-4 px-5 rounded-full border shadow hover:text-sky-500 duration-500 cursor-pointer ">
+                            <CiHeart className="items-center text-xl " />
+                        </div>
+                    </div>
+
+                    <div className="mt-5">
+                        <p className="font-medium my-2"><span className="font-semibold text-[#181D4E] cursor-pointer">Category:</span> {category}</p>
+
+                        <p className="font-medium my-2"><span className="font-semibold text-[#181D4E] ">Tags: </span>
+                         item,  play,  store</p>
+
+                        <p className="font-medium my-2 flex items-center gap-1"><span className="font-semibold text-[#181D4E] ">Ratings: </span>
+                         {rating} <FaStar  className="text-yellow-300" /></p>
+
+                        <p className="font-medium my-2 flex items-center gap-1"><span className="font-semibold text-[#181D4E] ">Quantity: </span>
+                         {quantity} <MdOutlineToys  className="" /></p>
+                    </div>
+
                 </div>
+
             </div>
         </div>
     );
